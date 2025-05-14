2fc54917c0d7fbc02cc73ccbf5f08350fff98105
THOMAS VIEIRA CLEMENTE <thomas_clemente@estudante.sesisenai.org.br>
2025-05-14 11:14:32 -0300

    atualização

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const {conectar, criarUsuario} = require('/test')
 const app = express()
 app.use(express.json())
 
-app.post = ('./usuario'(req,res));{
-    await criarUsuario('insert into usuario (nome, email, senha) values ($1, $2, $3)', [req.body.nome, req.body.email, req.body.senha]('ok'))
-}
+app.post = ('./usuario', async (req,res) => {
+    try{
+        const {nome, email, senha} = req.body
+        await criarUsuario(
+            'insert into usuario (nome, email) values ($1, $2, $3)', [req.body.nome, req.body.email, req.body.senha]('ok'))
+            [nome, email]
+    }catch (error){
+        msg_erro = "Internal Server Error"
+        let cod_erro = 500
+        if(error.no == 19){
+            msg_erro = "Email já cadastrado"
+            cod_erro = 400
+        }
+        res.status(cod_erro).json({error: msg})
+    }
+})
 
 conectar().them(()=>{
-    app.listen(8000, () => {
-        console.log('Servidor rodando na porta 8000')
+    app.listen(8000, async () => {
+        console.log('Servidor rodando')
     })
 })
